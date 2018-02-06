de36e9bf3dd884bf86be7efb51b371aa6bb45c44
Eliot Berriot <contact@eliotberriot.com>
2018-02-06 23:03:18 +0100

    Tests for parametrize

---
+++ tests/pytest/test_afpyro_parametrize.py
@@ -0,0 +1,12 @@
+import afpyro
+import pytest
+
+
+@pytest.mark.parametrize('edition, expected', [
+    (1, 'La Boate'),
+    (2, 'Zenith'),
+    (3, 'Stade Vélodrome'),
+    (4, 'Palais du Pharo'),
+])
+def test_afpyro_get_place(edition, expected):
+    assert afpyro.get_place(edition) == expected
